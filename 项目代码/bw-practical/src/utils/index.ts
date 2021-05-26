d117cd6c1db5810dd110e920556f73c9da91b8a6
赵晓杰 <1944775835@qq.com>
2021-05-26 08:51:33 +0800

    update project

---
+++ 项目代码/bw-practical/src/utils/index.ts
@@ -0,0 +1,16 @@
+interface RootObject {
+  stationId: string;
+  stationVersionId: string;
+  name: string;
+  majorId: string;
+  userId: string;
+  userName: string;
+  status: string;
+  skillNum: string;
+  majorName: string;
+  stationVersion: number;
+  stationLevelList?: any;
+  labelTreeList?: any;
+  skillList?: any;
+  createTime: string;
+}
