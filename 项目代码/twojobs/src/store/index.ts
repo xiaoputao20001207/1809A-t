ea33acd6f748522b9ff8a17983dfa2d1694d1d1a
dxy-mm <1372021607@qq.com>
2021-05-30 20:39:20 +0800

    'update'

--- 项目代码/twojobs/src/store/index.ts
+++ 项目代码/twojobs/src/store/index.ts
@@ -1,7 +1,9 @@
 //引入子模块
 import skill from './modules/skill'
 import description from './modules/description'
+import proSkill from './modules/proSkill'
 export default{
     skill,
-    description
+    description,
+    proSkill
 }
